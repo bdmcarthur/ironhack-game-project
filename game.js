f062c9baa4bdc79c535e17fbcc89b9efe79a37f4
Brandy McArthur <bdm.mcarthur@gmail.com>
2019-10-29 09:09:10 +0100

    Increased speed

--- game.js
+++ game.js
@@ -1,110 +1,108 @@
 let frame = 0;
 let level = 1;
 let accel = 1;
-let speed = 3 + accel;
+let speed = 4 + accel;
 let gameBeingPlayed = true;
 let level1Complete = true;
 let currentScore = 100;
 let groundObstaclesArr = [];
 var donutArr = [];
 const SOUNDS = {
-    background: "sounds/background.wav",
-    hit: "sounds/hit.mp3",
-    donut: "sounds/donut.mp3"
-  };
+  background: "sounds/background.wav",
+  hit: "sounds/hit.mp3",
+  donut: "sounds/donut.mp3"
+};
 
 class Game {
-    constructor (canvas) {
-        this.canvas = canvas;
-        this.context = this.canvas.getContext('2d');
-        this.sound = new Sound();
-        this.sound.loadSounds(SOUNDS, this);
-        this.score = new Score(this);
-        this.player = new Player(this);
-        this.groundObstacles = new GroundObstacles(this);
-        this.donut = new Donut(this);
-        this.background = new Background(this);
-        this.speed = 4;
-        this.level = 1;
-        this.callbacks = {
-            jump: () => this.player.jump(),
-        };
-        this.control = new Control(this.callbacks);
-        this.control.setKeyBindings();   
-    }
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.context = this.canvas.getContext("2d");
+    this.sound = new Sound();
+    this.sound.loadSounds(SOUNDS, this);
+    this.score = new Score(this);
+    this.player = new Player(this);
+    this.groundObstacles = new GroundObstacles(this);
+    this.donut = new Donut(this);
+    this.background = new Background(this);
+    this.speed = 4;
+    this.level = 1;
+    this.callbacks = {
+      jump: () => this.player.jump()
+    };
+    this.control = new Control(this.callbacks);
+    this.control.setKeyBindings();
+  }
 
-    draw () {
-        this.clear();
-        this.background.draw()
-        this.player.draw();
-        this.score.draw();
-    }
+  draw() {
+    this.clear();
+    this.background.draw();
+    this.player.draw();
+    this.score.draw();
+  }
 
-    start () {
-        this.draw()
-        this.loop()
-    }
+  start() {
+    this.draw();
+    this.loop();
+  }
 
-    clear () {
-        const width = this.canvas.width;
-        const height = this.canvas.height;
-        this.context.clearRect(0, 0, width, height); 
+  clear() {
+    const width = this.canvas.width;
+    const height = this.canvas.height;
+    this.context.clearRect(0, 0, width, height);
+  }
+
+  update() {
+    this.groundObstacles.update();
+    this.donut.update();
+    this.player.crashWithObstacles(groundObstaclesArr);
+    this.player.crashWithDonut(donutArr);
+  }
+
+  resetLevel() {
+    frame = 0;
+    groundObstaclesArr = [];
+    groundObstaclesArr = [];
+    this.background = new Background(this);
+    this.player = new Player(this);
+    tempSpriteCount = 0;
+    tempDeadCount = 0;
+    tempJumpCount = 0;
+  }
+
+  lose() {
+    gameBeingPlayed = false;
+    speed = 0;
+    this.groundObstacles.imageLink = "images/sprite/rat3.png";
+    gameover.style.display = "flex";
+  }
+
+  win() {
+    if (level === 3) {
+      winGame.style.display = "flex";
+      gameBeingPlayed = false;
+    } else {
+      gameBeingPlayed = false;
+      speed = 0;
+      this.groundObstacles.imageLink = "images/sprite/rat3.png";
+      levelDiv.style.display = "flex";
+    }
+  }
+
+  loop() {
+    this.sound.play("background", { volume: 0.2 });
+    if (gameBeingPlayed === true) {
+      if (currentScore === 0) {
+        this.lose();
       }
 
-    update () {
-        this.groundObstacles.update();
-        this.donut.update();
-        this.player.crashWithObstacles(groundObstaclesArr);
-        this.player.crashWithDonut(donutArr);
-    } 
+      if (this.background.dx < -1155 && this.background.dx > -1210) {
+        this.win();
+      }
 
-    resetLevel() {
-        frame = 0;
-        groundObstaclesArr = [];
-        groundObstaclesArr = [];
-        this.background = new Background(this);
-        this.player = new Player(this);
-        tempSpriteCount = 0;
-        tempDeadCount = 0;
-        tempJumpCount = 0;   
+      frame += 1;
+      this.draw();
+      this.update();
+      window.requestAnimationFrame(() => this.loop());
     }
-
-    lose () {
-        gameBeingPlayed = false;
-        speed = 0;
-        this.groundObstacles.imageLink = 'images/sprite/rat3.png';
-        gameover.style.display = "flex"; 
-    }
-
-    win () {
-        if(level === 3){
-            winGame.style.display = "flex";
-            gameBeingPlayed = false;    
-        }
-        else{
-            gameBeingPlayed = false;
-            speed = 0;
-            this.groundObstacles.imageLink = 'images/sprite/rat3.png';
-            levelDiv.style.display = "flex";
-        }
-    }
-    
-    loop () {
-        this.sound.play('background', { volume: 0.2 })
-        if(gameBeingPlayed === true){
-        if(currentScore === 0){
-            this.lose();
-        }
-
-        if(this.background.dx < -1155 && this.background.dx > -1210){
-            this.win();     
-        }
-
-        frame += 1;
-        this.draw();
-        this.update();
-        window.requestAnimationFrame(() => this.loop());
-        }
-    }
-
+  }
 }
--- main.js
+++ main.js
@@ -1,49 +1,49 @@
-const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
 var width = canvas.width;
 var height = canvas.height;
 const game = new Game(canvas);
-const startButton = document.getElementById("start-button")
-const levelButton = document.getElementById("level-button")
-const gameoverButton = document.getElementById("gameover-button")
-const restartButton = document.getElementById("restart-button")
-const introDiv = document.getElementById("game-intro")
-const levelDiv = document.getElementById("level")
-const gameOver = document.getElementById("gameover")
-const winGame = document.getElementById("win-game")
+const startButton = document.getElementById("start-button");
+const levelButton = document.getElementById("level-button");
+const gameoverButton = document.getElementById("gameover-button");
+const restartButton = document.getElementById("restart-button");
+const introDiv = document.getElementById("game-intro");
+const levelDiv = document.getElementById("level");
+const gameOver = document.getElementById("gameover");
+const winGame = document.getElementById("win-game");
 
-  startButton.addEventListener('click',() => {
-    introDiv.classList.add('hide')
-    game.start();
-  });  
+startButton.addEventListener("click", () => {
+  introDiv.classList.add("hide");
+  game.start();
+});
 
-  levelButton.addEventListener('click',() => {
-    levelDiv.style.display = "none";
-    game.resetLevel()
-    level += 1;
-    accel += 1;
-    speed = 4 + accel
-    gameBeingPlayed = true;
-    game.loop();
-  });  
+levelButton.addEventListener("click", () => {
+  levelDiv.style.display = "none";
+  game.resetLevel();
+  level += 1;
+  accel += 1;
+  speed = 5 + accel;
+  gameBeingPlayed = true;
+  game.loop();
+});
 
-  gameoverButton.addEventListener('click',() => {
-    gameover.style.display = "none";
-    game.resetLevel()
-    currentScore = 100;  
-    level = 1;
-    accel = 1;
-    speed = 4; 
-    gameBeingPlayed = true; 
-    game.loop();
-  });  
+gameoverButton.addEventListener("click", () => {
+  gameover.style.display = "none";
+  game.resetLevel();
+  currentScore = 100;
+  level = 1;
+  accel = 1;
+  speed = 5;
+  gameBeingPlayed = true;
+  game.loop();
+});
 
-  restartButton.addEventListener('click',() => {
-    winGame.style.display = "none";
-    game.resetLevel()
-    level = 1;
-    accel = 1;
-    speed = 4; 
-    gameBeingPlayed = true; 
-    game.loop();
-  }); 
+restartButton.addEventListener("click", () => {
+  winGame.style.display = "none";
+  game.resetLevel();
+  level = 1;
+  accel = 1;
+  speed = 5;
+  gameBeingPlayed = true;
+  game.loop();
+});
